36a115c173ef95ede1cc2a88bacef15aa0fa0cc3
myin975 <156113006+myin975@users.noreply.github.com>
2024-03-08 22:18:52 -0500

    Add files via upload

---
+++ chap10/scripts/script.js
@@ -0,0 +1,20 @@
+/* 
+Author: Myles Yin Choy 
+File Name: script.js
+Date: 03/07/24 
+*/ 
+
+//Hamburger Menu Fuction 
+
+function Hamburger(){
+    var menu = document.getElementById("nav__link"); 
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    }   else{
+        menu.style.display = "block"; 
+    }
+} 
+
+
+
+
