c0ac4ff7f7c97f1a5962eda2ba5c4f2fc75a08d9
myin975 <156113006+myin975@users.noreply.github.com>
2024-03-24 23:12:30 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,29 @@ function Hamburger(){
     }   else{
         menu.style.display = "block"; 
     }
+}  
+
+//Function to display the first answer 
+function ans1(){ 
+    heading.style.display = "block"; 
+    answer.textContent = "Trial and error... and error and error...";
 } 
+
+//Function to display the second answer 
+function ans2(){
+    heading.style.display = "block"; 
+    answer.textContent = "My favorite color is blue!";
+} 
+
+//Function to display the third answer 
+function ans3(){
+    heading.style.display = "block"; 
+    answer.textContent = "I like running, going to the gym and drawing in my free time."; 
+}
+
+//Function to display the fourth answer 
+function ans4(){ 
+    heading.style.display = "block"; 
+    answer.textContent = "Feel free to add me on Discord."; 
+}
 
